a214651cedf2e41e3c03b830eaf8a14fe1cbc8a8
Kyle Fransen <privekylefransen@hotmail.com>
2021-02-20 21:10:09 +0100

    Added AI debt balance changes
    - Changed AI factor on buildings
    - Improved AI defines
    - Added disband mercs event to prevent mercs to sprial an AI into debt

---
+++ common/defines/EUPlusPlus_Defines.lua
@@ -0,0 +1,33 @@
+NDefines.NDiplomacy.INCREASE_TRUST_AMOUNT = 10 --Increased from 5
+NDefines.NDiplomacy.PREPARE_FOR_WAR_COST = 5 --Decreased from 10
+NDefines.NDiplomacy.FAVOR_GAIN_FOR_HELP = 25 --Increased from 20
+
+NDefines.NDiplomacy.PREPARE_FOR_WAR_MONTHS = 24 --Increased from 12
+NDefines.NDiplomacy.PEACE_COST_GOLD_STEP = 10 --Increased cost of gold demand
+NDefines.NDiplomacy.PEACE_COST_GOLD_MAX = 3 --Decreased from 5, to prevent countries to absolutely demolish the economy of the enemy
+NDefines.NAI.PEACE_WAR_EXHAUSTION_FACTOR = 1.2 --Increased from 1.0
+NDefines.NAI.PEACE_HIGH_WAR_EXHAUSTION_THRESHOLD = 8 --Increased from 10
+NDefines.NAI.PEACE_FORCE_BALANCE_FACTOR = 0 --Decreased from 0.2
+
+NDefines.NDiplomacy.CHARTER_COMPANY_BASE_COST = 1000000 --Increased from 1000
+
+NDefines.NCountry.ALLOWED_TERRITORY_VS_MAX_STATES = 80.0 --Increased from 20
+
+
+NDefines.NAI.POWERBALANCE_DISABLE_VERSUS_PLAYER = 1 --Increased from 0
+NDefines.NAI.DIPLOMATIC_ACTION_ALLIANCE_POWERBALANCE_FACTOR = 35 --Decreased from 50
+NDefines.NAI.DIPLOMATIC_ACTION_GUARANTEE_POWERBALANCE_FACTOR = 60 --Decreased from 80
+NDefines.NAI.OVER_FORCELIMIT_AVOIDANCE_FACTOR = 20 --Increased from 10
+
+NDefines.NAI.INCOME_SAVINGS_FRACTION = 0.5 --Increased from 0.25
+NDefines.NAI.DESIRED_SURPLUS = 0.35 --Increased from 0.1
+NDefines.NAI.MAX_SAVINGS = 100 --Increased from 60
+--NDefines.NAI.DEFICIT_SPENDING_MIN_MONTHS = 2
+--NDefines.NAI.DEFICIT_SPENDING_MIN_MONTHS_PEACETIME = 12
+NDefines.NAI.DEVELOPMENT_CAP_BASE = 50 --Incrased from 10
+
+
+NDefines.NAI.RIVAL_ALLIANCE_PENALTY = 100 -- Increased from 50
+
+NDefines.NCountry.ABDICATE_AGE_THRESHOLD = 40 --Decreased from 60
+NDefines.NAI.INVADING_MAX_AWAY_RATIO = 0.60 --Increased from 0.45
---
+++ descriptor.mod
@@ -0,0 +1,12 @@
+version="0.0.1"
+tags={
+	"Balance"
+	"Events"
+	"Gameplay"
+	"Missions And Decisions"
+	"National Ideas"
+	"Military"
+	"New Nations"
+}
+name="EU++"
+supported_version="1.30.5"
